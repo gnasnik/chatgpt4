28e1bc6d4c6369dbccd302bd136dd748260b841b
gnasnik <frankffenn@gmail.com>
2023-10-10 10:31:13 +0800

    update chatgpt3.5 url

--- src/components/main/Announcement.tsx
+++ src/components/main/Announcement.tsx
@@ -3,7 +3,7 @@ export default () => {
         <div class="text-sm text-gray-600 py-1 ">
         🎉🎉🎉
         <div>由于GPT4价格持续上涨，使用成本不断增加</div>
-        <div>为了支持更多人，我们现在开放免费通道： 👉<a href="https://chat-dev.chinaproxy.top" class="underline hv-base hover:bg-white"> ChatGPT3.5</a>，您可以选择混合使用。</div>
+        <div>为了支持更多人，我们现在开放免费通道： 👉<a href="https://free2.chinaproxy.top" class="underline hv-base hover:bg-white"> ChatGPT3.5</a>，您可以选择混合使用。</div>
         <div>如果您有能力，请优先使用GPT4，这将有助于我们为那些需要免费服务的人提供支持。</div>
         <div>想了解GPT3.5和GPT4.0之间的区别，<a target="_blank" href="https://n0a6ea0bnj9.feishu.cn/docx/Na7AdeXM8oWjUCxjBP7clDRmnle" class="underline">请点击这里。</a></div>
         </div>
